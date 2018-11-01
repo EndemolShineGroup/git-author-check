90fe70f90d3c7ecc738b887d539b1d4997d5c121
Hassan Khan <hassan.khan@endemolshine.com>
2018-11-01 00:17:50 +0000

    style(lint): fix TSLint error
    This is an issue with TSLint since the ECMAScript spec does not allow trailing
    commas on destructured rest parameters

--- src/getAuthorEmail.ts
+++ src/getAuthorEmail.ts
@@ -3,6 +3,7 @@ import { spawnSync } from 'child_process';
 export default (): string => {
   const [
     firstArg,
+    // tslint:disable-next-line:trailing-comma
     ...remainingArgs
   ]: string[] = 'git config --get user.email'.split(' ');
 
